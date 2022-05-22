0a8bfca79ef0a1c3f788089de000df94bfa8d32b
Eric <ericlg.link@gmail.com>
2022-05-22 01:55:37 -0700

    Added js functionality to nav

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// MENU ELEMENTS
+const navMenu = document.getElementById('nav_menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+// SHOW MENU
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+// HIDE MENU
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+//REMOVE MENU
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
